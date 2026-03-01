0df17674620558d9dd74686cd10a02e35f5600e0
martinmilenkvski <51443462+martinmilenkvski@users.noreply.github.com>
2026-03-01 18:43:39 +0100

    chore: install Vite, React, Tailwind and setup project skeleton

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-500">
+        Kon-Trans Ignition Successful
+      </h1>
+    </div>
+  );
+}
+
+export default App;
